d35615ca922ba06e49c5cc2b7ee9e14bb8ec2be1
= <=>
2019-04-27 16:32:48 -0700

    DBInterface improvements, new db file

--- DBInterface.py
+++ DBInterface.py
@@ -1,5 +1,6 @@
 
 from datetime import date, datetime, timedelta
+import decimal
 import sqlite3
 import time
 
@@ -8,7 +9,7 @@ from os.path import join, abspath
 # Project Imports
 from DataCollector import getTopCryptos, getHistoricalDataDaily
 
-NUM_CRYTPOS = 35
+NUM_CRYTPOS = 100
 
 # dir must already exist
 DB_DIR = 'db'
@@ -26,14 +27,16 @@ c = conn.cursor()
 aggr = {}  # To aggregate total value of all NUM_CRYPTOS for each date
 today_price = {}
 
+decimal.getcontext().prec = 40
+
 def pop_marketcap():
    # Create marketcap table
    c.execute('''CREATE TABLE marketcap
                (coin text, mkt_cap real, time_collected text)''')
    for crypto in top_cryptos:
-      mkt_cap = today_price.get(crypto) / aggr.get(str(date.today()))
+      mkt_cap = decimal.Decimal(today_price.get(crypto)) / aggr.get(str(date.today()))
       ts = get_ts()
-      string = "INSERT INTO marketcap VALUES ('" + str(crypto) + "', " + str(mkt_cap) + ", '" + str(ts) + "')"
+      string = "INSERT INTO marketcap VALUES ('" + str(crypto.replace('*', '')) + "', " + str(mkt_cap) + ", '" + str(ts) + "')"
       c.execute(string)
 
 def pop_aggregate():
@@ -45,35 +48,31 @@ def pop_aggregate():
 
 def pop_cryptos():
    for crypto in top_cryptos:
-      # Create table for each NUM_CRYTOS
-      try:
-         c.execute('CREATE TABLE ' + crypto +
-                  '''(date text, close real, high real, low real, open real, time_collected text)''')
-      except sqlite3.OperationalError:
-         break # Just skip this one: Wierd symbol name like HOT*
+      c.execute('CREATE TABLE ' + crypto.replace('*', '') +  # Special char in SQL, remove it
+               '''(date text, close real, high real, low real, open real, time_collected text)''')
 
-      historData = getHistoricalDataDaily(crypto, all=True)
+      historData = getHistoricalDataDaily(crypto, 500)
 
       days_ago = 0 # Keep track of days past for aggregate table
       for histDate in historData: 
-         if days_ago == 0: # Update latest price (use for market cap)
-            today_price.update( {crypto: histDate['open']})
+         if days_ago == 0: # Update latest price (use for market percentage)
+            today_price.update( {crypto: histDate['close']})
 
          datestamp = str(date.today() - timedelta(days=days_ago)) # ISO-8601 date
-         
+
          # update aggregate amount based on existing value an closing value of current crypto
-         if len(aggr) > 0 and aggr.get(datestamp) is not None:
+         if aggr.get(datestamp) is not None:
             # sum up with existing aggregated data
-            aggr.update( {datestamp: (aggr.get(datestamp) + histDate['close']) } )
+            total = decimal.Decimal(decimal.Decimal(aggr.get(datestamp)) + decimal.Decimal(histDate['close']))
+            aggr.update( {datestamp: total } )
          else :  # first entry for this date
             aggr.update({datestamp: histDate['close']}) 
-      
-         estr = 'INSERT INTO ' + crypto + " VALUES ('" + datestamp + "', " + \
+       
+         c.execute('INSERT INTO ' + crypto.replace('*', '') + " VALUES ('" + datestamp + "', " + \
             str(histDate['close']) + ', ' + str(histDate['high']) + ', ' + \
-            str(histDate['low']) + ', ' + str(histDate['open']) + ", '" + get_ts() +  "' )"
-         
-         c.execute( estr )
+            str(histDate['low']) + ', ' + str(histDate['open']) + ", '" + get_ts() +  "' )" )
          days_ago += 1
+
 def main():
    pop_cryptos()
    pop_aggregate()
